a026c55a887723a031779ed0d6e4139b9ad48d74
Sacha <38068324+qwykx@users.noreply.github.com>
2025-11-12 09:30:12 +0100

    Change helm-charts registry path to lippdatacloud

--- .dagger/main.go
+++ .dagger/main.go
@@ -169,7 +169,7 @@ func (m *Sftpgo) Release(
 
 	err := m.build(ctx, version).
 		WithRegistryAuth("ghcr.io", githubActor, githubToken).
-		Publish(ctx, fmt.Sprintf("oci://ghcr.io/%s/helm-charts", githubAccount))
+		Publish(ctx, fmt.Sprintf("oci://ghcr.io/lippdatacloud/helm-charts", githubAccount))
 	if err != nil {
 		return err
 	}
